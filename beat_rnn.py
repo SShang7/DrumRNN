89528f54dc9016afbe4bd42fbdae81e6ba222666
Sammy Shang <sammy7shang@gmail.com>
2022-08-09 16:39:45 -0700

    midi support for drum data

--- beat_rnn.py
+++ beat_rnn.py
@@ -32,6 +32,7 @@ sample_file = filenames[1]
 
 #print(sample_file)
 
+pm = pretty_midi.PrettyMIDI(sample_file)
 
 def play_music(midi_filename):
   '''Stream music_file in a blocking manner'''
@@ -52,14 +53,81 @@ pygame.mixer.init(freq, bitsize, channels, buffer)
 # optional volume 0 to 1.0
 pygame.mixer.music.set_volume(0.8)
 
+'''
+for i, note in enumerate(instrument.notes[:10]):
+  drum_name = pretty_midi.note_number_to_drum_name(note.pitch)
+  duration = note.end - note.start
+  print(f'{i}: note_name={drum_name},'
+        f' duration={duration:.4f}')
+'''
+instrument = pm.instruments[0]
+instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['velocity'].append(note.velocity)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+raw_notes = midi_to_notes(sample_file)
+print(raw_notes.head())
+
+def notes_to_midi(
+  notes: pd.DataFrame,
+  out_file: str, 
+  instrument_name: str,
+) -> pretty_midi.PrettyMIDI:
+
+  pm = pretty_midi.PrettyMIDI()
+  instrument = pretty_midi.Instrument(
+      program=pretty_midi.instrument_name_to_program(
+          instrument_name),
+      is_drum=True)
+
+  prev_start = 0
+  for i, note in notes.iterrows():
+    start = float(prev_start + note['step'])
+    end = float(start + note['duration'])
+    note = pretty_midi.Note(
+        velocity=int(note['velocity']),
+        pitch=int(note['pitch']),
+        start=start,
+        end=end,
+    )
+    instrument.notes.append(note)
+    prev_start = start
+
+  pm.instruments.append(instrument)
+  pm.write(out_file)
+  return pm
+
+example_file = 'example.midi'
+example_pm = notes_to_midi(
+    raw_notes, out_file=example_file, instrument_name=instrument_name)
+
 # listen for interruptions
 try:
   # use the midi file you just saved
-  play_music(sample_file)
+  play_music(example_file)
 except KeyboardInterrupt:
   # if user hits Ctrl/C then exit
   # (works only in console mode)
   pygame.mixer.music.fadeout(1000)
   pygame.mixer.music.stop()
   raise SystemExit
-
